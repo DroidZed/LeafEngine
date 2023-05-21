47c214aa8f257c6ba8c4c42e7b2b8be99d596aca
DroidZed <41507665+DroidZed@users.noreply.github.com>
2023-05-21 16:42:49 +0100

    Forgot to install pytest 🤡

--- leaf_engine/skeleton.py
+++ leaf_engine/skeleton.py
@@ -1,9 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from .utils._singleton import SingletonClass
 
-class Skeleton:
+
+class Skeleton(meta=SingletonClass):
     def __init__(self, db_url: str, db_name: str):
-        self.__db_instance = AsyncIOMotorClient(db_url)[db_name]
+        self.__db_instance = AsyncIOMotorClient(db_url)[
+            db_name
+        ]
+
+    @property
+    def db_instance(self):
+        return self.__db_instance
+
 
 # Copied from my Bot's code: https://www.github.com/DroidZed/Z-Butler
 #
@@ -12,17 +20,17 @@ class Skeleton:
 #         self.__db_connection = AsyncIOMotorClient(
 #             MDB_SRV, serverSelectionTimeoutMS=5000
 #         )[DB_NAME]
-# 
+#
 #     @property
 #     def db_connection(self):
 #         return self.__db_connection
-# 
-# 
+#
+#
 # class MongoDBHelperClient:
 #     """
 #     A helper classes used as a wrapper for all CRUD methods against the mongodb database.
 #     """
-# 
+#
 #     def __init__(self, collection_name: str):
 #         self._mdb_connection = (
 #             MongoDBConnection().db_connection
@@ -30,7 +38,7 @@ class Skeleton:
 #         self._collection = self._mdb_connection[
 #             collection_name
 #         ]
-# 
+#
 #     async def query_collection(
 #         self, payload: dict
 #     ) -> list[dict] | None:
@@ -38,28 +46,28 @@ class Skeleton:
 #         A CRUD method used to query a specific collection using the payload given in arguments.
 #         Args:
 #             payload: dict
-# 
+#
 #         Returns:
 #                 A list of documents fetched from the database. None if nothing is present.
 #         """
-# 
+#
 #         return [
 #             c
 #             async for c in self._collection.find(
 #                 payload, {"_id": 0, "__v": 0}
 #             )
 #         ]
-# 
+#
 #     async def insert_into_collection(
 #         self, payload: list[dict]
 #     ) -> None:
 #         await self._collection.insert_many(payload)
-# 
+#
 #     async def delete_from_collection(
 #         self, payload: dict
 #     ) -> None:
 #         await self._collection.delete_many(payload)
-# 
+#
 #     async def update_document(
 #         self,
 #         criteria: dict,
